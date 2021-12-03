014732d8de8d8ef55422e293f9fe5fa97f455756
js816 <82730954+js816@users.noreply.github.com>
2021-12-03 11:19:47 -0600

    Adding DB and updated parsing

---
+++ DB/QuickDBD-export.sql
@@ -0,0 +1,23 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "music_instrument" (
+    "reviewerID" text   NOT NULL,
+    "asin" text   NOT NULL,
+    "reviewerName" text   NOT NULL,
+    "reviewText" text   NOT NULL,
+    "overall" double   NOT NULL,
+    "summary" text   NOT NULL,
+    "unixReviewTime" bigint   NOT NULL,
+    "reviewTime" text   NOT NULL,
+    "helpful_yes" bigint   NOT NULL,
+    "helpful_no" bigint   NOT NULL,
+    CONSTRAINT "pk_music_instrument" PRIMARY KEY (
+        "reviewerID","asin"
+     )
+);
+
